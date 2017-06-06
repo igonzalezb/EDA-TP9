33c0ebb08d329c469d688f6e97f4957b19d80461
Iñaki <igonzalez@itba.edu.ar>
2017-06-06 18:10:16 -0300

    SUBIDO

--- LCD 1-2/main.cpp
+++ LCD 1-2/main.cpp
@@ -3,47 +3,13 @@
 //
 // Instituto Tecnologico de Buenos Aires  
 //
-// Algoritmos y Estructuras de Datos - Trabajo Práctico 9 
+// Algoritmos y Estructuras de Datos - Trabajo Práctico 10 
 //
 //
 // Gonzalez Bigliardi Iñaki			    /////////////////////////////////////////
-// Lago Valentina				   // Proyecto LCD - Fases I y II
+// Lago Valentina				   // Proyecto LCD - Fase III
 // Muller Malena				  //////////////////////////////////////////
 //
-//
-//  El presente trabajo práctico utiliza con un display de cuarzo
-// líquido (LCD) alfanumérico de 2x16 caracteres.
-// El objetivo del mismo es crear una librería que permita manejar el LCD y contenga el
-// driver del LCD al igual que una capa de abstracción (HAL) que le permitirán a quien 
-// la utilice manipular el display.
-//
-// Proyecto Fase I 
-//
-// *Asignación de nombres a los pines del puerto 
-// *Definir todas las instrucciones del LCD
-// *Escribir las siguientes funciones de bajo nivel:
-//
-//  FT_HANDLE * deviceHandler lcdInit(int iDevice); //Inicializa el FTDI y el LCD
-//
-//  void lcdWriteIR(FT_HANDLE * deviceHandler, BYTE valor); //Escribe un byte al IR
-//
-//  void lcdWriteDR(FT_HANDLE * deviceHandler, BYTE valor); //Escribe un byte al DR
-//
-// Proyecto Fase II
-//  
-//  En esta etapa montaremos una capa sobre la anterior que nos permita una mejor
-// comunicación con las capas superiores.
-//
-//  Abajo se incluye el prototipo de una clase abstracta a implementar junto con una
-// descripción de cada una de las funciones que la componen.
-//
-//  Implementamos la clase basicLCD como una clase abstracta y creamos una clase concreta 
-// a partir de basicLCD que contenga todas las implementaciones de las funciones descriptas 
-// en basicLCD para el caso particular del display Hitachi HD44780.
-//
-//  Ademas realizamos un programa de prueba (main) que permita probar una
-// instancia (objeto) de la clase heredada de basicLCD. 
-// 
 // 
 /////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -85,6 +51,7 @@ int main(int argc, char * argv[])
 			allegroShutdown();
 			return EXIT_FAILURE;
 		}
+		
 		double speed = 2.0;
 		ALLEGRO_TIMER * timer = NULL;
 		timer = al_create_timer(speed);
